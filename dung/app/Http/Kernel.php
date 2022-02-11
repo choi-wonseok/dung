58170c7635577f0ddd690b10187775e61de4b14a
WonseokChoi <apples226@hotmail.com>
2022-02-12 01:56:46 +0900

    change

--- dung/app/Http/Kernel.php
+++ dung/app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'auth.dung' => \App\Http\Middleware\AuthDung::class,
-        'forcessl' => \App\Http\Middleware\ForceSSL::class
+        'auth.dung' => \App\Http\Middleware\AuthDung::class
     ];
 }
