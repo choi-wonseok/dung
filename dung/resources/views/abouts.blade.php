2cdb81ca9c28e2db68899572a40b7c2b61eeec0c
WonseokChoi <apples226@hotmail.com>
2022-02-14 00:19:48 +0900

    front

---
+++ dung/resources/views/abouts.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Item - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="/css/jeje.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <!-- Product section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="row gx-4 gx-lg-5 align-items-center">
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="/image/ddong.jpg" alt="..." /></div>
+                    <div class="col-md-6">
+                        <h4 class="display-5 fw-bolder">급똥엔젤에 대해...</h4>
+
+                        <p class="lead"> 처음 가보는 길을 걷다가, 혹은 아는 길이라도, 정말 급하셨던 적 다들 있으시죠? <br> 너무 급해서 이걸 어떻게 해야하나 안절부절했던 적 있으시죠? <br> 저희 급똥엔젤은 그런 여러분들을 위해 제작되었습니다. <br> 언제 어디서나 급똗엔젤에 접속을 하면 자신의 위치에서 가장 가까운 화장실의 위치가 나오고, 그 화장실까지의 거리, 시간, 경로가 나오도록 제작을 하였습니다. <br> 당신이 정말 필요할때 당신 손 안에 작은 천사, 급똥엔젤, 당신을 구해드립니다.
+
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+        <footer class="py-5 bg-darks">
+            <div class="container"><p class="m-0 text-center text-white">X세대 &copy; 최현 최원석 임근웅 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <!-- Core theme JS-->
+        <script src="js/scr.js"></script>
+    </body>
+</html>
