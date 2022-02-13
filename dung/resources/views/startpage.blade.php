c9f94e6650f59748eda01d64d2640a36f289df9d
woongs9 <limgw91@gmail.com>
2022-02-14 01:36:41 +0900

    map test 7

--- dung/resources/views/startpage.blade.php
+++ dung/resources/views/startpage.blade.php
@@ -42,7 +42,7 @@
             </div>
         </div>
 
-        <a href="nmap://route/walk?slat=null&slng=null&sname=null&dlat=37.4764356&dlng=126.9618302&dname=null&appname=https://x-angels.ml" id="serach1">@if (count($rows)){{ $rows[0]->toiletName }}@endif</a>
+        <a href="nmap://route/walk?slat=null&slng=null&sname=null&dlat=37.4764356&dlng=126.9618302&dname=화장실&appname=https://x-angels.ml" id="serach1">@if (count($rows)){{ $rows[0]->toiletName }}@endif</a>
 
         <a href="plustoilet" id="serach2"></a>
     </div>
