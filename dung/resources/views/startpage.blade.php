e82533772963954d054be8a3620cb102d94df5f9
woongs9 <limgw91@gmail.com>
2022-02-14 01:32:59 +0900

    map test 5

--- dung/resources/views/startpage.blade.php
+++ dung/resources/views/startpage.blade.php
@@ -42,7 +42,7 @@
             </div>
         </div>
 
-        <a href="nmap://route/walk?slat=37.4640070&slng=126.9522394&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=37.4764356&dlng=126.9618302&dname=%EB%8F%99%EC%9B%90%EB%82%99%EC%84%B1%EB%8C%80%EC%95%84%ED%8C%8C%ED%8A%B8&appname=https://x-angels.ml" id="serach1">@if (count($rows)){{ $rows[0]->toiletName }}@endif</a>
+        <a href="nmap://route/walk?slat=&slng=&sname=&dlat=37.4764356&dlng=126.9618302&dname=&appname=https://x-angels.ml" id="serach1">@if (count($rows)){{ $rows[0]->toiletName }}@endif</a>
 
         <a href="plustoilet" id="serach2"></a>
     </div>
